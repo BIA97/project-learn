8c0bdb400be09e1aa35fe37ddfe7f774a349f805
Ivan Burkovskyi <vnja97bia@gmail.com>
2024-04-21 10:27:23 +0300

    create project and see 16 lesson

---
+++ js/example.js
@@ -0,0 +1,31 @@
+var firstVariable = 1;
+let secondVariable = 2;
+const thirdVariable = 3;
+//  snake_case = 'variable';
+//  UPPER_SNAKE_CASE = 'variable';
+//  Kebab-case = 'type variable';
+// PascalCase = 'tyype variable';
+
+
+//                           next 11 lesson
+const myArray = [1, 2, 3, 4, 5, 'name', { a: 155, money: 500 }]; // array 
+console.log(myArray[5]);  //name
+console.log(myArray[6].a);  //155
+console.log(myArray[6].money);  //500
+
+
+myArray[7] = 'ads new value'
+console.log(myArray.length);
+console.log(myArray[7]);
+
+
+
+
+const myObject = {
+    name: 'Ivan',
+    age: 26,
+    phone: '+380972701031'
+}  //object
+console.log(myObject.phone);  //+380972701031
+myObject.nws = 150
+console.log(myObject);
